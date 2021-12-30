1d557e0ac15644967ab490ffa9b9b08e30b8a517
HeBianGu <908293466@qq.com>
2021-12-30 18:33:30 +0800

    1111

---
+++ Solution/HeBianGu.Systems.actionnuget/IdentityService.cs
@@ -0,0 +1,23 @@
+﻿
+//using HeBianGu.Base.WpfBase;
+//using System;
+//using System.Threading;
+
+//namespace HeBianGu.Systems.actionnuget
+//{
+//    public class IdentityService : IIdentityService
+//    {
+//        Random random = new Random();
+//        public bool Login(string name, string password, out string message)
+//        {
+//            Thread.Sleep(2000);
+//            message = "操作超时";
+//            return random.Next(2) == 1;
+//        }
+
+//        public bool Register(string name, string phone, string password)
+//        {
+//            return true;
+//        }
+//    }
+//}
